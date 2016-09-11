2550178335afc6effbfc2c038a835f9fbc969890
Jasper Tomas <jaspertomas@gmail.com>
2016-09-11 10:54:01 +0800

    initial commit

---
+++ src/typingtutor/Constants.java
@@ -0,0 +1,340 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typingtutor;
+
+/**
+ *
+ * @author jaspertomas
+ */
+public class Constants {
+    String lessonkeys[]=
+    {
+        "asdf" ,
+        "jkl;" ,
+        "asdfghjkl;" ,
+        "qwert" ,
+        "yuiop" ,
+        "qwertyuiop" ,
+        "asdfghjkl;qwertyuiop" ,
+        "zxcvb" ,
+        "nm,." ,
+        "zxcvbnm,." ,
+        "asdfghjkl;qwertyuiopzxcvbnm,."
+    };
+
+    String lessonsongs[]=
+    {
+        "\"Heal The World\"\n" +
+        "\n" +
+        "There's A Place In\n" +
+        "Your Heart\n" +
+        "And I Know That It Is Love\n" +
+        "And This Place Could\n" +
+        "Be Much\n" +
+        "Brighter Than Tomorrow\n" +
+        "And If You Really Try\n" +
+        "You'll Find There's No Need\n" +
+        "To Cry\n" +
+        "In This Place You'll Feel\n" +
+        "There's No Hurt Or Sorrow\n" +
+        "\n" +
+        "There Are Ways\n" +
+        "To Get There\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Little Space\n" +
+        "Make A Better Place...\n" +
+        "\n" +
+        "Heal The World\n" +
+        "Make It A Better Place\n" +
+        "For You And For Me\n" +
+        "And The Entire Human Race\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+        "\n" +
+        "If You Want To Know Why\n" +
+        "There's A Love That\n" +
+        "Cannot Lie\n" +
+        "Love Is Strong\n" +
+        "It Only Cares For\n" +
+        "Joyful Giving\n" +
+        "If We Try\n" +
+        "We Shall See\n" +
+        "In This Bliss\n" +
+        "We Cannot Feel\n" +
+        "Fear Or Dread\n" +
+        "We Stop Existing And\n" +
+        "Start Living\n" +
+        "\n" +
+        "Then It Feels That Always\n" +
+        "Love's Enough For\n" +
+        "Us Growing\n" +
+        "So Make A Better World\n" +
+        "Make A Better World...\n" +
+        "\n" +
+        "Heal The World\n" +
+        "Make It A Better Place\n" +
+        "For You And For Me\n" +
+        "And The Entire Human Race\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+        "\n" +
+        "And The Dream We Were\n" +
+        "Conceived In\n" +
+        "Will Reveal A Joyful Face\n" +
+        "And The World We\n" +
+        "Once Believed In\n" +
+        "Will Shine Again In Grace\n" +
+        "Then Why Do We Keep\n" +
+        "Strangling Life\n" +
+        "Wound This Earth\n" +
+        "Crucify Its Soul\n" +
+        "Though It's Plain To See\n" +
+        "This World Is Heavenly\n" +
+        "Be God's Glow\n" +
+        "\n" +
+        "We Could Fly So High\n" +
+        "Let Our Spirits Never Die\n" +
+        "In My Heart\n" +
+        "I Feel You Are All\n" +
+        "My Brothers\n" +
+        "Create A World With\n" +
+        "No Fear\n" +
+        "Together We'll Cry\n" +
+        "Happy Tears\n" +
+        "See The Nations Turn\n" +
+        "Their Swords\n" +
+        "Into Plowshares\n" +
+        "\n" +
+        "We Could Really Get There\n" +
+        "If You Cared Enough\n" +
+        "For The Living\n" +
+        "Make A Little Space\n" +
+        "To Make A Better Place...\n" +
+        "\n" +
+        "Heal The World\n" +
+        "Make It A Better Place\n" +
+        "For You And For Me\n" +
+        "And The Entire Human Race\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+        "\n" +
+        "Heal The World\n" +
+        "Make It A Better Place\n" +
+        "For You And For Me\n" +
+        "And The Entire Human Race\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+        "\n" +
+        "Heal The World\n" +
+        "Make It A Better Place\n" +
+        "For You And For Me\n" +
+        "And The Entire Human Race\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+        "\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+        "\n" +
+        "There Are People Dying\n" +
+        "If You Care Enough\n" +
+        "For The Living\n" +
+        "Make A Better Place\n" +
+        "For You And For Me\n" +
+
+        "\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "You And For Me\n" +
+        "\n" +
+        "Heal the world we live in, save it for our children\n" +
+        "Heal the world we live in, save it for our children\n" +
+        "Heal the world we live in, save it for our children\n" +
+        "Heal the world we live in, save it for our children "+
+
+        "\"You Are Not Alone\"\n" +
+        "\n" +
+        "Another day has gone\n" +
+        "I'm still all alone\n" +
+        "How could this be\n" +
+        "You're not here with me\n" +
+        "You never said goodbye\n" +
+        "Someone tell me why\n" +
+        "Did you have to go\n" +
+        "And leave my world so cold\n" +
+        "\n" +
+        "Everyday I sit and ask myself\n" +
+        "How did love slip away\n" +
+        "Something whispers in my ear and says\n" +
+        "That you are not alone\n" +
+        "I am here with you\n" +
+        "Though you're far away\n" +
+        "I am here to stay\n" +
+        "\n" +
+        "But you are not alone\n" +
+        "I am here with you\n" +
+        "Though we're far apart\n" +
+        "You're always in my heart\n" +
+        "But you are not alone\n" +
+        "\n" +
+        "'Lone, 'lone\n" +
+        "Why, 'lone\n" +
+        "\n" +
+        "Just the other night\n" +
+        "I thought I heard you cry\n" +
+        "Asking me to come\n" +
+        "And hold you in my arms\n" +
+        "I can hear your prayers\n" +
+        "Your burdens I will bear\n" +
+        "But first I need your hand\n" +
+        "Then forever can begin\n" +
+        "\n" +
+        "Everyday I sit and ask myself\n" +
+        "How did love slip away\n" +
+        "Something whispers in my ear and says\n" +
+        "That you are not alone\n" +
+        "I am here with you\n" +
+        "Though you're far away\n" +
+        "I am here to stay\n" +
+        "\n" +
+        "you are not alone\n" +
+        "I am here with you\n" +
+        "Though we're far apart\n" +
+        "You're always in my heart\n" +
+        "you are not alone\n" +
+        "\n" +
+        "Whisper three words and I'll come runnin'\n" +
+        "And girl you know that I'll be there\n" +
+        "I'll be there\n" +
+        "\n" +
+        "You are not alone\n" +
+        "I am here with you\n" +
+        "Though you're far away\n" +
+        "I am here to stay\n" +
+        "you are not alone\n" +
+        "I am here with you\n" +
+        "Though we're far apart\n" +
+        "You're always in my heart\n" +
+        "\n" +
+        "you are not alone\n" +
+        "I am here with you\n" +
+        "Though you're far away\n" +
+        "I am here to stay\n" +
+        "\n" +
+        "you are not alone\n" +
+        "I am here with you\n" +
+        "Though we're far apart\n" +
+        "You're always in my heart\n" +
+        "\n" +
+        "For you are not alone...\n" +
+        "Not alone ohh\n" +
+        "You are not alone\n" +
+        "You are not alone\n" +
+        "Say it again\n" +
+        "You are not alone\n" +
+        "You are not alone\n" +
+        "Not alone, Not alone\n" +
+        "If you just reach out for me girl\n" +
+        "In the morning, in the evening\n" +
+        "Not alone, not alone\n" +
+        "You and me not alone\n" +
+        "Oh together together\n" +
+        "Not not being alone\n" +
+        "Not not being alone \n"+
+        
+        "\"Human Nature\"\n" +
+        "\n" +
+        "Looking out" +
+        "Across the nighttime" +
+        "The city winks a sleepless eye" +
+        "Hear her voice" +
+        "Shake my window" +
+        "Sweet seducing sighs" +
+        "" +
+        "Get me out" +
+        "Into the nighttime" +
+        "Four walls won't hold me tonight" +
+        "If this town" +
+        "Is just an apple" +
+        "Then let me take a bite" +
+        "" +
+        "If they say," +
+        "Why, why, tell 'em that it's human nature" +
+        "Why, why, does he do it that way" +
+        "If they say," +
+        "Why, why, tell 'em that it's human nature" +
+        "Why, why does he do me that way" +
+        "" +
+        "Reaching out" +
+        "To touch a stranger" +
+        "Electric eyes are everywhere" +
+        "See that girl" +
+        "She knows I'm watching" +
+        "She likes the way I stare" +
+        "" +
+        "If they say," +
+        "Why, why, tell 'em that it's human nature" +
+        "Why, why, does he do me that way" +
+        "If they say," +
+        "Why, why, tell 'em that it's human nature" +
+        "Why, why does he do me that way" +
+        "" +
+        "I like livin' this way" +
+        "I like lovin' this way" +
+        "" +
+        "(That way) Why why" +
+        "(That way) Why why" +
+        "" +
+        "Looking out" +
+        "Across the morning" +
+        "Where the city's heart begins to beat" +
+        "Reaching out" +
+        "I touch her shoulder" +
+        "I'm dreaming of the street" +
+        "" +
+        "If they say," +
+        "Why, why, tell 'em that it's human nature" +
+        "Why, why, does he do me that way" +
+        "If they say," +
+        "Why, why, ooo tell 'em" +
+        "Why, why does he do me that way" +
+        "If they say why, why, cha cha cha cha cha cha" +
+        "Why does he do me that way" +
+        "If they say why, why, why, ooo tell 'em" +
+        "Why does he do me that way" +
+        "If they say why, ooo tell 'em" +
+        "Why does he do me that way" +
+        "If they say why, da da da da da da da da" +
+        "Why does he do my that way, I like living this way" +
+        "Why, oh why, why, why" 
+    };
+}
